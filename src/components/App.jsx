16a0f54861f67ca98f1c5583e1f12ad367f3d9a0
YevhenBez <byxgalter@bigmir.net>
2023-06-26 20:34:21 +0300

    Перенёс логику работу телефонной книги в файл    Contacts.js   и применил маршруты. Модуль 8   REACT
    Перенёс логику работу телефонной книги в файл    Contacts.js    из файла    App.jsx    При этом переименовал функцию на  Contacts Это сделано для того, чтобы когда пользователь залогинится, то у него отображалась телефонная книга.  А также применил простые (не защищённые)  маршруты просто для того, чтобы не было ошибок и рендерилась телефонная книга. Для этого в файле    App.jsx   импортнул    Routes, Route   и   lazy    и сделал импорт файла    Contacts    при помощи   функции     lazy    В функции же   App вернул    <Routes>    в котором прописал   Route   где указал страницу   <Contacts />     и затем экспортнул данную функцию     App   При этом обрати внимание, что закоментированный код со строки 17 и ниже это как была написана функция Арр  под Бэкенд    mockapi.io
      Модуль 8   REACT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,53 @@
-import ContactForm from './contactForm/ContactForm';
-import ContactList from './contactList/ContactList';
-import Filter from './Filter/Filter';
-import Loader from './loader/Loader';
-import { useSelector } from 'react-redux';
-import { selectIsLoading } from '../redux/selectors';
+import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+const Contacts = lazy(() => import('../pages/contacts/Contacts'));
 
 const App = () => {
-  const isLoadingApp = useSelector(selectIsLoading);
-
-  console.log(`Проверка isLoading ${isLoadingApp}`);
-
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      {/* React homework template */}
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        {isLoadingApp && <Loader />}
-        <ContactList />
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Contacts />}/>
+      </Routes>
     </div>
-  );
-};
+  )
+}
 
 export default App;
+
+// import ContactForm from './contactForm/ContactForm';
+// import ContactList from './contactList/ContactList';
+// import Filter from './Filter/Filter';
+// import Loader from './loader/Loader';
+// import { useSelector } from 'react-redux';
+// import { selectIsLoading } from '../redux/selectors';
+
+// const App = () => {
+//   const isLoadingApp = useSelector(selectIsLoading);
+
+//   console.log(`Проверка isLoading ${isLoadingApp}`);
+
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       {/* React homework template */}
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm />
+//         <h2>Contacts</h2>
+//         <Filter />
+//         {isLoadingApp && <Loader />}
+//         <ContactList />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
