ab69df8a9dbebe9639e684dc54bebd2e14ded1fd
YevhenBez <byxgalter@bigmir.net>
2023-06-27 12:38:12 +0300

    На страничке  Home   добавил иконку и настроил цвета. Модуль 8    REACT
    На страничке  Home   добавил иконку и настроил цвета. Для этого импортнул иконку  AutoStoriesIcon  и вставил данную иконку в разметку при помощи    <AutoStoriesIcon color="primary" sx={{ width: '100%', height: '70vh' }} />    При этом обрати внимание, что команды    color="primary"   (которая позволяет задать цвет элементу) и    sx={{ width: '100%', height: '70vh' }}    (которая позволяет задать ширину, высоту элемента и т.п.)    это команды чисто библиотеки    Material   UI    (см. документацию библиотеки    Material   UI    )    Модуль 8    REACT

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
 import MaterialUI from '../../components/MaterialUI';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 
 const Home = () => {
     return (
         <>
-            <MaterialUI.Typography
+            <MaterialUI.Typography color="primary"
         variant="h2"
         sx={{ fontSize: '10vw', textAlign: 'center' }}
       >
         This is Phonebook
-      </MaterialUI.Typography>
+            </MaterialUI.Typography>
+            <AutoStoriesIcon color="primary" sx={{ width: '100%', height: '70vh' }} />
         </>
     );
 };
