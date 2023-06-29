1aeb7736bd09c60bf63d1278646fa0cb93677a34
YevhenBez <byxgalter@bigmir.net>
2023-06-29 12:40:17 +0300

    Создал функцию    setToken   для прописания Токена  и функцию    deleteToken    для удаления Токена. Модуль 8    REACT
    Создал функцию    setToken   для прописания Токена. Для этого использовал    axios.defaults.headers.common.Authorization   это дефолтные настройки   axios    (см. документацию на ресурсе    https://axios-http.com/docs/config_defaults     ). А также использовал здесь    `Bearer ${token}`    этот кусочек кода здесь всегда такой для прописания Токена.    Также создал функцию    deleteToken    для удаления Токена. Она просто прописывает вместо Токена пустое значение в дефолтных настройках   axios Модуль 8    REACT

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,3 +1,10 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const setToken = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+const deleteToken = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
